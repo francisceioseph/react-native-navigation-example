0e5bf40172cb4ca4c15f7f9ccb9792049eb23139
Francisco Souza <osf.fjose@creditas.com>
2021-09-09 10:54:01 -0300

    feat: remove unused imports

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import {StatusBar, StyleSheet, View} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import {StyleSheet, View} from 'react-native';
 
 type TPageProps = {
   children: React.ReactNode;
